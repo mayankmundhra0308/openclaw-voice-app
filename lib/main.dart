a6a8a3bbf54c8fe0241c5b56e423412469895f16
mayankmundhra0308 <mayankmundhraupsc@gmail.com>
2026-03-22 19:23:30 +0530

    fix: register Hive adapters, request mic permission, add generated .g.dart, skip build_runner

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'models/thread.dart';
 import 'screens/home_screen.dart';
 import 'screens/settings_screen.dart';
 import 'theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Init Hive
   await Hive.initFlutter();
-  await Hive.openBox('threads');
-  await Hive.openBox('messages');
+
+  // Register adapters
+  if (!Hive.isAdapterRegistered(0)) Hive.registerAdapter(ThreadAdapter());
+  if (!Hive.isAdapterRegistered(1)) Hive.registerAdapter(MessageAdapter());
+
+  // Open boxes
+  await Hive.openBox<Thread>('threads');
+  await Hive.openBox<Message>('messages');
+
+  // Request mic permission upfront
+  await Permission.microphone.request();
+
   runApp(const OpenClawVoiceApp());
 }
 
